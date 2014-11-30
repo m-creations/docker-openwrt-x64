572b02a8ad1aa22c5ad44e2b647a581069747d42
Kambiz Darabi <darabi@m-creations.net>
2014-11-30 18:39:12 +0100

    Added build script for updating rootfs
    Note that the docker build has nothing to do
    with this build script. Only the rootfs tar
    which is referenced in the Dockerfile is changed
    by build.sh

---
+++ bin/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+release=$1
+
+wget https://github.com/m-creations/openwrt/releases/download/$release/openwrt-x86_64-rootfs.tar.gz
+
+mv -f openwrt-x86_64-rootfs.tar.gz image/
+
+rm -rf image/tmp
+
+mkdir image/tmp
+
+tar xzv -C image/tmp -f image/openwrt-x86_64-rootfs.tar.gz 
+
+mkdir image/tmp/tmp/{log,lock}
+
+rm image/tmp/lib/modules/*/*
+
+cat <<EOF>> image/tmp/root/.bashrc
+source /etc/profile
+
+alias ll='ls -al'
+alias po=popd
+alias pu=pushd
+alias md=mkdir
+EOF
+
+rm image/openwrt-x86_64-rootfs.tar.gz
+
+tar czv -C image/tmp -f image/openwrt-x86_64-rootfs.tar.gz .
+
+rm -rf image/tmp/
+
+docker build -t mcreations/openwrt-x64:$release .
+
+docker run -it --rm mcreations/openwrt-x64:$release opkg update
+
+git tag -f -m "Tagging $release" -a $release
